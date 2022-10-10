28f96caa39df6583669700e69abad8b6bb8e3ce9
Andrew Fox <90468039+bobthecorn2000@users.noreply.github.com>
2022-10-10 10:47:44 -0600

    10/10 backup

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using Windows.Storage;
+using Microsoft.Web.WebView2.Core;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow 
+    {
+        Uri myUri = new Uri("https://www.google.com", UriKind.Absolute);
+        public MainWindow()
+        {
+        
+            InitializeComponent();
+            InitializeAsync();
+        }
+        private void ButtonGo_Click(object sender, RoutedEventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                if (addressBar.Text.Substring(0, 8) == "https://" || addressBar.Text.Substring(0, 7) == "http://")
+                {
+                    webView.CoreWebView2.Navigate(addressBar.Text);
+                }
+                else webView.CoreWebView2.Navigate("https://"+addressBar.Text);
+
+            }
+        }
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            addressBar.Text = uri;
+           // webView.CoreWebView2.PostWebMessageAsString(uri);
+        }
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
+        }
+        private void tbCtrl_Loaded(object sender, RoutedEventArgs e)
+        {
+            tbControl = (sender as TabControl);
+            testname = (sender as TabItem);
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+           /* TabItem newTabItem = new TabItem
+            {
+                Header = "Test",
+                Name = "Test"
+                 
+            };
+            tbControl.Items.Add(newTabItem);*/
+             WebView2 webBrowser3 = new WebView2
+            {
+               
+                Source = myUri,
+                
+            };
+            tbControl.Items.Add(webBrowser3);
+
+        }
+    }
+}
---
+++ carloan2/carloan2/Program.cs
@@ -0,0 +1,90 @@
+﻿//tyler forsgren
+//10/6/2022
+using static System.Console;
+class DemoLoan
+{
+    static void Main()
+    {
+
+        Loan aLoan = new Loan();
+        CarLoan aCarLoan = new CarLoan();
+        aLoan.LoanNumber = 2239;
+        aLoan.LastName = "Mitchell ";
+        aLoan.LoanAmount = 1_000.00;
+        aCarLoan.LoanNumber = 3358;
+        aCarLoan.LastName = "Jansen";
+        aCarLoan.LoanAmount = 20_000.00;
+        aCarLoan.Make = "Ford";
+        aCarLoan.Year = 2007;
+        WriteLine("Loan #{0} for {1} is for {2}", aLoan.LoanNumber, aLoan.LastName, aLoan.LoanAmount.ToString("C2"));
+        WriteLine("Loan #{0} for {1} is for {2}", aCarLoan.LoanNumber, aCarLoan.LastName, aCarLoan.LoanAmount.ToString("C2"));
+        WriteLine(" Loan #{0} is for a {1} {2}", aCarLoan.LoanNumber, aCarLoan.Year, aCarLoan.Make);
+    }
+}
+
+
+
+class Loan
+{
+    public const double MINIMUM_LOAN = 5_000;
+    protected double loanAmount;
+    public int LoanNumber
+    {
+        get;
+        set;
+    }
+    public string LastName
+    {
+        get;
+        set;
+    }
+    public double LoanAmount
+    {
+        set
+        {
+            if (value < MINIMUM_LOAN)
+                loanAmount = MINIMUM_LOAN;
+            else loanAmount = value;
+        }
+        get
+        {
+            return loanAmount;
+        }
+    }
+
+}
+
+class CarLoan : Loan
+{
+    private const int EARLIEST_YEAR = 2008;
+    private const int LOWEST_INVALID_NUM = 1000;
+    private int year;
+    public int Year { 
+        set { 
+            if (value < EARLIEST_YEAR) { 
+                year = value; loanAmount = 0; 
+            } else year = value; 
+        } 
+
+        get { 
+            return year; 
+        }
+    }
+public new int LoanNumber{   
+        get   {     
+            return base.LoanNumber;
+        }
+        set  {     
+            if(value < LOWEST_INVALID_NUM)        
+                base.LoanNumber = value;      
+            else         
+                base.LoanNumber = value % LOWEST_INVALID_NUM;   
+        }
+    }
+    public string Make
+    {
+        get;
+        set;
+    }
+}
+
